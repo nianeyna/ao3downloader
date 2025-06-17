1503e683b2cd913b0b4568201b44c91c72bbf77b
nianeyna <nianeyna@gmail.com>
2025-06-16 21:58:52 -0700

    Fix bash function output

--- publish.sh
+++ publish.sh
@@ -5,15 +5,15 @@ get_new_version() {
     local api_url="$1"
     local version_format="^([0-9]{4})\.([0-9]{1,2})\.([0-9]+)"
 
-    echo "Getting current repository version"
+    echo "Getting current repository version" >&2
     local version=$(uv version --short)
     
     if [[ ! "$version" =~ ${version_format} ]]; then
-        echo "ERROR: Unexpected repository version format $version"
+        echo "ERROR: Unexpected repository version format $version" >&2
         exit 1
     fi
 
-    echo "Checking for existing package version at $api_url"
+    echo "Checking for existing package version at $api_url" >&2
 
     local api_response=$(curl -s -w "HTTPSTATUS:%{http_code}" "$api_url")
     local api_response_body=$(echo "$api_response" | sed -e 's/HTTPSTATUS\:.*//g')
@@ -22,46 +22,46 @@ get_new_version() {
     local api_version
     if [ "$api_response_status" -eq 200 ]; then
         api_version=$(echo "$api_response_body" | jq -r '.info.version')
-        echo "Found existing package version: $api_version"
+        echo "Found existing package version: $api_version" >&2
     elif [ "$api_response_status" -eq 404 ]; then
         api_version="0.0.0"
-        echo "No existing package version found"
+        echo "No existing package version found" >&2
     else
-        echo "ERROR: Status code when fetching package version from api was $api_response_status"
+        echo "ERROR: Status code when fetching package version from api was $api_response_status" >&2
         exit 1
     fi
 
     if [[ ! "$api_version" =~ ${version_format} ]]; then
-        echo "ERROR: Unexpected existing package version format $api_version"
+        echo "ERROR: Unexpected existing package version format $api_version" >&2
         exit 1
     fi
 
-    echo "Comparing current repository version ($version) with existing package version ($api_version)"
+    echo "Comparing current repository version ($version) with existing package version ($api_version)" >&2
     
     local max_version=$(echo -e "$version\n$api_version" | sort -V | tail -n1)
-    echo "Maximum existing version is $max_version"
+    echo "Maximum existing version is $max_version" >&2
 
     IFS='.' read -r max_year max_month max_patch <<< "$max_version"
     local max_version_prefix="${max_year}.${max_month}"
-    echo "Maximum existing version date is $max_version_prefix"
+    echo "Maximum existing version date is $max_version_prefix" >&2
 
     local new_version_prefix=$(date -u +%Y.%-m)
-    echo "The current UTC date is $new_version_prefix"
+    echo "The current UTC date is $new_version_prefix" >&2
 
     local new_version
     if [[ "${max_version_prefix}" > "${new_version_prefix}" ]]; then
-        echo "ERROR: The current UTC date (${new_version_prefix}) is less " \
-            "than the maximum existing version date (${max_version_prefix})"
+        echo "ERROR: The current UTC date (${new_version_prefix}) is less" \
+             "than the maximum existing version date (${max_version_prefix})" >&2
         exit 1
     elif [[ "${max_version_prefix}" == "${new_version_prefix}" ]]; then
-        echo "The current UTC date (${new_version_prefix}) is equal to " \
-            "the maximum existing version date (${max_version_prefix})"
-        echo "Incrementing the patch version"
+        echo "The current UTC date (${new_version_prefix}) is equal to" \
+             "the maximum existing version date (${max_version_prefix})" >&2
+        echo "Incrementing the patch version" >&2
         new_version="${new_version_prefix}.$((10#$max_patch + 1))"
     else
-        echo "The current UTC date (${new_version_prefix}) is greater than " \
-            "the maximum existing version date (${max_version_prefix})"
-        echo "Setting the patch version to 0"
+        echo "The current UTC date (${new_version_prefix}) is greater than" \
+             "the maximum existing version date (${max_version_prefix})" >&2
+        echo "Setting the patch version to 0" >&2
         new_version="${new_version_prefix}.0"
     fi
 
@@ -80,20 +80,25 @@ else
 fi
 
 new_version=$(get_new_version "$api_url")
+echo "Setting version to $new_version"
 uv version "$new_version"
 
+echo "Committing version bump to branch $branch_name"
 git config user.name "github-actions[bot]"
 git config user.email "github-actions[bot]@users.noreply.github.com"
 git add pyproject.toml
+git add uv.lock
 git commit -m "Bump version to $new_version"
 
 git push origin HEAD:"$branch_name"
 git pull origin "$branch_name"
 
+echo "Creating tag for version $new_version"
 tag_name="${tag_prefix}${new_version}"
 git tag -a "$tag_name" -m "$tag_message"
 git push origin "$tag_name"
 
+echo "Building and publishing package"
 uv build
 if [ "$is_test" = "true" ]; then
     uv publish --index testpypi
